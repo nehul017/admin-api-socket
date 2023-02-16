43e8f59175571339418f37ad6ed9544bdb6512a9
Nehul dalwala <nehul.dalwala@gmail.com>
2023-02-16 22:01:26 +0530

    login and register page added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { router } = require("./routers");
+const cors = require("cors");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/test")
@@ -12,6 +13,9 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// CORS is enabled for all origins
+app.use(cors());
 
 app.use("/", router);
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,8 +8,9 @@ class userController {
 
       const user = await userService.getOneUser({ email: body.email });
 
-      if (user) throw new Error("User already exiting");
-
+      if (user) {
+        return res.status(404).send({ message: "User already exiting" });
+      }
       body.password = md5(body.password);
 
       const newUser = await userService.create(body);
@@ -27,12 +28,12 @@ class userController {
       const user = await userService.getOneUser({ email: body.email });
 
       if (!user) {
-        return res.json({ message: "User not found" });
+        return res.status(404).send({ message: "User not found" });
       } else if (user.password === md5(body.password)) {
         var token = jwt.sign({ id: user._id }, "shhhhh");
         return res.json({ data: user, token });
       } else {
-        return res.json({ message: "password is not match" });
+        return res.status(400).send({ message: "password is not match" });
       }
     } catch (error) {
       console.log("error", error);
